ee5fa32dc47b26e4b4498bf5136ddbb5cd77882d
jazmarie624 <jazmarie624@gmail.com>
2026-09-21 07:12:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Supabase > Project Settings > API Keys > anon public.
 window.JES_CONFIG = {
   SUPABASE_URL: "https://ejseathlglvvnmkpchib.supabase.co",
-  SUPABASE_ANON_KEY: "PASTE_ANON_KEY_HERE"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqc2VhdGhsZ2x2dm5ta3BjaGliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5ODc4NTUsImV4cCI6MjA5NzU2Mzg1NX0.o4crd1W5-NbVNXSOWNT-WPHS7R6orRe9CRYFZfZ41L8"
 };
